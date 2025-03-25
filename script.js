3908389fc3b8c490e9835379bf7569ffd8ed1052
Aman Malik <163856210+Aman017@users.noreply.github.com>
2025-03-25 21:17:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function removeColor() {
+    let select = document.getElementById("colorSelect");
+    let selectedOption = select.options[select.selectedIndex];
+    
+    if (selectedOption) {
+        select.remove(select.selectedIndex);
+    }
+}
